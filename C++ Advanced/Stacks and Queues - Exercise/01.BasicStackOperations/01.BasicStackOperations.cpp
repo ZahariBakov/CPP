273823cd825b3561ff065a25c225439a25cc10c9
ZahariBakov <zaharibakov@gmail.com>
2023-10-27 16:57:09 +0300

    Create folder "Stacks and Queues - Exercise". Add exercises "01.BasicStackOperations" and "02.BasicQueueOperations".

---
+++ C++ Advanced/Stacks and Queues - Exercise/01.BasicStackOperations/01.BasicStackOperations.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <limits>
+
+std::stack<int> readInput(int& numbersToRemove, int& targetNumber) {
+    std::stack<int> numbers;
+    int inputNumbersCount = 0;
+
+    std::cin >> inputNumbersCount;
+    std::cin >> numbersToRemove;
+    std::cin >> targetNumber;
+
+    int currentNumber = 0;
+
+    for (int i = 0; i < inputNumbersCount; ++i) {
+        std::cin >> currentNumber;
+        numbers.push(currentNumber);
+    }
+
+    return numbers;
+}
+
+void removeElement(const int numbersToRemove, std::stack<int>& stackNumbers) {
+    if (stackNumbers.size() < (size_t)numbersToRemove) {
+        // sanity check
+        std::cerr << "Invalid input, stack size is: " << stackNumbers.size() 
+                  << ", while numberToRemove is: " << numbersToRemove << std::endl;
+    }
+
+    for (int i = 0; i < numbersToRemove; ++i) {
+        stackNumbers.pop();
+    }
+}
+
+std::string findSolution(const int targetNumber, std::stack<int>& stackNumbers) {
+    std::string result;
+
+    if (stackNumbers.empty()) {
+        return "0";
+    }
+
+    int currentNumber = 0;
+    int smallesNumber = std::numeric_limits<int>::max();
+
+    while (!stackNumbers.empty()) {
+        currentNumber = stackNumbers.top();
+
+        if (currentNumber == targetNumber) {
+            return "true";
+        }
+
+        if (smallesNumber > currentNumber) {
+            smallesNumber = currentNumber;
+        }
+
+        stackNumbers.pop();
+    }
+
+    return std::to_string(smallesNumber);
+}
+
+void printSolution(const std::string& answer) {
+    std::cout << answer << std::endl;
+}
+
+int main() {
+    int numbersToRemove = 0; 
+    int targetNumber = 0;
+
+    std::stack<int> stackNumbers = readInput(numbersToRemove, targetNumber);
+    removeElement(numbersToRemove, stackNumbers);
+    const std::string result = findSolution(targetNumber, stackNumbers);
+    printSolution(result);
+
+    return 0;
+}
