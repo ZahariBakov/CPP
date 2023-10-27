f6019bbda880285e73b4a2c239a2efbe5cc57557
ZahariBakov <zaharibakov@gmail.com>
2023-10-27 11:46:36 +0300

    Add exercise "05.PriorityQueue" and "06.UsingDeque".

---
+++ C++ Advanced/Stacks and Queues - Lab/05.PriorityQueue/05.PriorityQueue.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+int main() {
+    std::priority_queue<int> pQueue;
+    pQueue.push(13);
+    pQueue.push(69);
+    pQueue.push(42);
+
+    pQueue.pop();
+    pQueue.push(201);
+    pQueue.push(20);
+
+    while (!pQueue.empty()) {
+        std::cout << pQueue.top() << std::endl;
+        pQueue.pop();
+    }
+
+    std::cout << "=======================" << std::endl;
+
+    std::priority_queue<int, std::deque<int>, std::greater<int>> reversedQueue;
+    reversedQueue.push(13);
+    reversedQueue.push(69);
+    reversedQueue.push(42);
+
+    while (!reversedQueue.empty()) {
+        std::cout << reversedQueue.top() << std::endl;
+        reversedQueue.pop();
+    }
+
+    return 0;
+}
