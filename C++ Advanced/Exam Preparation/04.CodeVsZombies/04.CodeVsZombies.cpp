02cab4cc5b97086cc837f974005e51042850f4da
ZahariBakov <zaharibakov@gmail.com>
2023-11-28 15:34:16 +0200

    Update additional exercise "04.CodeVsZombies". Passed the all tests except 18. The tactic is to save the nearest human.

--- C++ Advanced/Exam Preparation/04.CodeVsZombies/04.CodeVsZombies.cpp
+++ C++ Advanced/Exam Preparation/04.CodeVsZombies/04.CodeVsZombies.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 struct Point {
     int x;
@@ -23,6 +24,35 @@ public:
     void readInput() {
         std::cin >> pos.x >> pos.y;
     }
+
+    void calculateDistances(const std::vector<Unit>& humans,
+                            const std::vector<Zombie>& zombies) {
+        // Humans distances
+        const size_t humansCount = humans.size();
+        mHumanDistances.resize(humansCount);
+
+        for (size_t i = 0; i < humansCount; ++i) {
+            const int deltaX = pos.x - humans[i].pos.x;
+            const int deltaY = pos.y - humans[i].pos.y;
+            const double currDistance = sqrt((deltaX * deltaX) + (deltaY * deltaY));
+            mHumanDistances[i] = currDistance;
+        }
+
+        // Zombies distances
+        const size_t zombiesCount = zombies.size();
+        mZombieDistances.resize(zombiesCount);
+
+        for (size_t i = 0; i < zombiesCount; ++i) {
+            const int deltaX = pos.x - zombies[i].pos.x;
+            const int deltaY = pos.y - zombies[i].pos.y;
+            const double currDistance = sqrt((deltaX * deltaX) + (deltaY * deltaY));
+            mZombieDistances[i] = currDistance;
+        }
+    }
+
+    size_t getClosestHumanIdx() {
+        return std::min_element(mHumanDistances.begin(), mHumanDistances.end()) - mHumanDistances.begin();
+    }                           // This returns an iterator to the smallest Idx - // This retun smallest Idx
 
 private:
     std::vector<double> mHumanDistances;
@@ -56,31 +86,47 @@ public:
             std::cin >> zombie.futurePos.x;
             std::cin >> zombie.futurePos.y;
         }
+
+        if (humansCount == 0 || zombiesCount == 0) {
+            mIsRuning = false;
+        }
     }
 
     void calculateDistances() {
-
+        mAsh.calculateDistances(mHumans, mZombies);
     }
 
     void act() {
-        std::cout << "8000 5000" << std::endl;
+        // Find nearset human.
+        const size_t clossestHumanIdx = mAsh.getClosestHumanIdx();
+        const Unit& clossestHuman = mHumans[clossestHumanIdx];
+        std::cout << clossestHuman.pos.x << " " << clossestHuman.pos.y << std::endl;
+        
+        // This is static coordinates for first solution
+        //std::cout << "8000 5000" << std::endl;
     }
 
     bool isRuning() {
-        return true;
+        return mIsRuning;
     }
 
 private:
     Ash mAsh;
     std::vector<Unit> mHumans;
     std::vector<Zombie> mZombies;
+    bool mIsRuning = true;
 };
 
 int main() {
     Game game;
 
-    while (game.isRuning()) {
+    while (true) {
         game.readInput();
+
+        if (!game.isRuning()) {
+            break;
+        }
+
         game.calculateDistances();
         game.act();
     }
