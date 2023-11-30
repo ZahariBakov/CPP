f8025accc4115916c079da2f4196a06fb59c188e
ZahariBakov <zaharibakov@gmail.com>
2023-11-30 14:21:00 +0200

    Add exercise "02.SecondLecture". Add skeleton to exercise "05.Memory".

---
+++ C++ OOP/Objects and Classes - Exercise/05.Memory/CompanyMemoryUtils.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+
+#include "Company.h"
+
+std::vector<Company> readCompaniesFromMemory(unsigned char* memory, int numCompanies) {
+	std::vector<Company> companies;
+
+	return companies;
+}
