92b18a586334536a6422dcc6e59f48cd30c18c27
ZahariBakov <zaharibakov@gmail.com>
2023-12-01 09:40:42 +0200

    Add solution for exercise "05.Memory".

--- C++ OOP/Objects and Classes - Exercise/05.Memory/CompanyMemoryUtils.h
+++ C++ OOP/Objects and Classes - Exercise/05.Memory/CompanyMemoryUtils.h
@@ -1,11 +1,45 @@
 #pragma once
 
 #include <vector>
+#include <cstring>
 
 #include "Company.h"
 
 std::vector<Company> readCompaniesFromMemory(unsigned char* memory, int numCompanies) {
 	std::vector<Company> companies;
+	char* localMemory = reinterpret_cast<char*>(memory);
+	unsigned char id = 0;
+	unsigned char employeesCount = 0;
+	std::string name;
+	std::vector<std::pair<char, char> > employees;
+
+	for (int i = 0; i < numCompanies; ++i) {
+		id = *localMemory;
+		localMemory += sizeof(id);
+
+		const size_t nameLen = strlen(localMemory);
+		name = std::string(localMemory, nameLen);
+		localMemory += (nameLen + 1);
+
+		employeesCount = *localMemory;
+		localMemory += sizeof(employeesCount);
+
+		employees.clear();
+
+		for (unsigned char employeeId = 0; employeeId < employeesCount; ++employeeId) {
+			std::pair<char, char> currEmployee;
+
+			currEmployee.first = *localMemory;
+			localMemory += sizeof(currEmployee.first);
+
+			currEmployee.second = *localMemory;
+			localMemory += sizeof(currEmployee.second);
+
+			employees.push_back(currEmployee);
+		}
+
+		companies.emplace_back(id, name, employees);
+	}	
 
 	return companies;
 }
