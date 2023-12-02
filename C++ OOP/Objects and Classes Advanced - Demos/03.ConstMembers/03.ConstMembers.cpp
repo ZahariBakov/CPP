aa45e6be97e86f5b3c7bd40579a53e2644c0229d
ZahariBakov <zaharibakov@gmail.com>
2023-12-02 10:20:08 +0200

    Add demos "03.ConstMembers" and "04.PitfallForgettingToMarkMethodConst".

---
+++ C++ OOP/Objects and Classes Advanced - Demos/03.ConstMembers/03.ConstMembers.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+class Company {
+private:
+    long long capitalDollars;
+public:
+    const std::string id;
+
+    Company(std::string id, long long capitalDollars) 
+        : capitalDollars(capitalDollars), id(id) {}
+
+    void addCapital(long long dollars) {
+        this->capitalDollars += dollars;
+    }
+
+    void print() const {
+        std::cout << this->id << this->capitalDollars << std::endl;
+    }
+};
+
+int main() {
+    Company c{ "GOOGINC.", 100000000LL };
+    const Company& constRef = c;
+
+    std::cout << c.id << std::endl;
+    // c.id = "thiswontcompile"; 
+    // compilation error - attempting to modfy a const field
+
+    constRef.print();
+    c.addCapital(999999);
+    constRef.print();
+    // constRef.addCapital(999999);
+    // compilation error - calling non-const method on const reference
+
+    return 0;
+}
