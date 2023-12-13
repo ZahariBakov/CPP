b16f9af6f01995b328b28312f67ee044c92cb626
ZahariBakov <zaharibakov@gmail.com>
2023-12-13 10:25:02 +0200

    Create new folder "Code Organization and Templates - Lab". Add exercise "01. Compile".

---
+++ C++ OOP/Code Organization and Templates - Lab/01. Compile/Defines.h
@@ -0,0 +1,12 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#include <string>
+#include <iostream>
+
+#define DONT_COMPILE_THIS
+
+#define STANDARD_TEMPLATE_LIBRARY namespace std
+
+#endif // !DEFINES_H
+
