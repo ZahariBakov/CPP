01dcc832105805d848d44306f296d70ac387bec7
ZahariBakov <zaharibakov@gmail.com>
2023-12-13 17:32:25 +0200

    Add exercise "03. Parser".

---
+++ C++ OOP/Code Organization and Templates - Lab/03. Parser/PrintUtils.h
@@ -0,0 +1,15 @@
+#ifndef PRINTUTILS_H
+#define PRINTUTILS_H
+
+#include <vector>
+#include <iostream>
+
+template<typename T>
+void printVector(std::vector<T> v) {
+	for (T& element : v) {
+		std::cout << element << " ";
+	}
+}
+
+#endif // !PRINTUTILS_H
+
