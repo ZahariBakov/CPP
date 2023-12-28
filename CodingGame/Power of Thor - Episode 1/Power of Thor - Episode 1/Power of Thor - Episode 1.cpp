1000fcca443546a9a7fd782c0f92ff2f07e3a7d8
ZahariBakov <zaharibakov@gmail.com>
2023-12-28 07:20:47 +0200

    Adding a solution to "Power of Thor - Episode 1".

---
+++ CodingGame/Power of Thor - Episode 1/Power of Thor - Episode 1/Power of Thor - Episode 1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int light_x; // the X position of the light of power
+    int light_y; // the Y position of the light of power
+    int initial_tx; // Thor's starting X position
+    int initial_ty; // Thor's starting Y position
+    std::cin >> light_x >> light_y >> initial_tx >> initial_ty; 
+    std::cin.ignore();
+
+    int thorX = initial_tx;
+    int thorY = initial_ty;
+
+    // game loop
+    while (1) {
+        int remaining_turns; // The remaining amount of turns Thor can move. Do not remove this line.
+        std::cin >> remaining_turns; 
+        std::cin.ignore();
+
+        std::string directionX = "";
+        std::string directionY = "";
+
+        if (thorX > light_x) {
+            directionX = "W";
+            thorX--;
+        }
+        else if (thorX < light_x) {
+            directionX = "E";
+            thorX++;
+        }
+
+        if (thorY > light_y) {
+            directionY = "N";
+            thorY--;
+        }
+        else if (thorY < light_y) {
+            directionY = "S";
+            thorY++;
+        }
+
+        std::cout << directionY << directionX << std::endl;
+    }
+
+    return 0;
+}
