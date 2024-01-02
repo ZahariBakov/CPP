fab8b8814fd782b6bfab3e12ee585be1bbed80e0
ZahariBakov <zaharibakov@gmail.com>
2024-01-02 18:50:08 +0200

    Adding exercise "02. Remove Invalid".

---
+++ C++ OOP/Rule of 3-5-0 - Exercise/02. Remove Invalid/RemoveInvalid.h
@@ -0,0 +1,22 @@
+#ifndef REMOVE_INVALID_H
+#define REMOVE_INVALID_H
+
+#include "Company.h"
+
+void removeInvalid(std::list<Company*>& companies) {
+	auto i = companies.begin();
+
+	while (i != companies.end()) {
+		Company* c = *i;
+
+		if (c->getId() < 0) {
+			delete c;
+			i = companies.erase(i);
+		}
+		else {
+			i++;
+		}
+	}
+}
+
+#endif // !REMOVE_INVALID_H
