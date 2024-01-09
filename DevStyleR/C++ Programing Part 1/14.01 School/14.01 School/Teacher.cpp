13b2c062d7076e32b9aa294a2694ae6c8ddaebe6
ZahariBakov <zaharibakov@gmail.com>
2024-01-09 09:28:34 +0200

    Update project "C++ Programing Part 1".

--- DevStyleR/C++ Programing Part 1/14.01 School/14.01 School/Teacher.cpp
+++
@@ -1,9 +0,0 @@
-#include "Teacher.h"
-
-Teacher::Teacher(std::string name, std::string title, Discipline& disciple) 
-	: name(name), title(title), discipline(disciple) {}
-
-void Teacher::printTeacherInfo() {
-	std::cout << "?eacher name " << this->name << " title " << this->title << " and discipline "; 
-	this->discipline.printDisciplineIfno();
-}
