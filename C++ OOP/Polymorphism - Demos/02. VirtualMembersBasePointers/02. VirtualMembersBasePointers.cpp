5ae1d10f991c33e8f3ac4a5e4f0658c80c615915
ZahariBakov <zaharibakov@gmail.com>
2024-01-14 15:44:17 +0200

    Adding demo "02. VirtualMembersBasePointers".

---
+++ C++ OOP/Polymorphism - Demos/02. VirtualMembersBasePointers/02. VirtualMembersBasePointers.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+class Vehicle {
+protected:
+    double speed;
+
+    Vehicle(double speed) : speed(speed) {}
+
+public:
+    virtual std::string toString() const {
+        std::ostringstream stream;
+        stream << "speed: " << this->speed;
+
+        return stream.str();
+    }
+
+    virtual void stop() {
+        this->speed = 0;
+    }
+};
+
+class Car : public Vehicle {
+    bool parkingBrakeOn;
+public:
+    Car(double speed, bool parkingBrakeOn)
+        : Vehicle(speed), parkingBrakeOn(parkingBrakeOn) {}
+
+    std::string toString() const override {
+        std::ostringstream stream;
+        stream << Vehicle::toString() << " parking brake: " << (this->parkingBrakeOn ? "yes" : "no");
+
+        return stream.str();
+    }
+
+    virtual void stop() override {
+        Vehicle::stop();
+        this->parkingBrakeOn = true;
+    }
+};
+
+class Airplane : public Vehicle {
+    double altitude;
+    double heading;
+public:
+    Airplane(double speed, double altitude, double heading)
+        : Vehicle(speed), altitude(altitude), heading(heading) {}
+
+    virtual std::string toString() const override {
+        std::ostringstream stream;
+        stream << Vehicle::toString() << " altitude: " << this->altitude << " heading: " << this->heading;
+
+        return stream.str();
+    }
+
+    virtual void stop() override {
+        Vehicle::stop();
+        this->altitude = 0;
+    }
+};
+
+class StaticPlaygroundTrain : public Vehicle {
+public:
+    StaticPlaygroundTrain() : Vehicle(0) {}
+
+    virtual std::string toString() const override {
+        return "cho-choooo!";
+    }
+};
+
+int main() {
+    Airplane plane(510, 2400, 90);
+
+    Vehicle* v = &plane;
+
+    // calls Airplane::toString() due to override
+    std::cout << v->toString() << std::endl;
+    v->stop(); // calls Airplane::stop() due to override
+    std::cout << v->toString() << std::endl;
+
+    Car car(50, false);
+    v = &car;
+
+    // calls Car::toString() due to override
+    std::cout << v->toString() << std::endl;
+    v->stop(); // calls Car::stop() due to override
+    std::cout << v->toString() << std::endl;
+
+    StaticPlaygroundTrain train;
+    v = &train;
+
+    // calls StaticPlaygroundTrain::toString() due to override
+    std::cout << v->toString() << std::endl;
+
+    // calls Vehicle::toString() because there is no override
+    // for stop() in StaticPlaygroundTrain
+    v->stop();
+    std::cout << v->toString() << std::endl;
+
+    return 0;
+}
