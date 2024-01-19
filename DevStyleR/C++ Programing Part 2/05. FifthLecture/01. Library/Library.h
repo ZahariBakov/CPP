54a7ff76d82357702685ad0699262bbd5e152960
ZahariBakov <zaharibakov@gmail.com>
2024-01-19 13:26:11 +0200

    Update exercise "Library".

---
+++ DevStyleR/C++ Programing Part 2/05. FifthLecture/01. Library/Library.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Book.h"
+
+class Library : public Book {
+public:
+	Library();
+};
--- DevStyleR/C++ Programing Part 2/05. FifthLecture/01. Library/main.cpp
+++ DevStyleR/C++ Programing Part 2/05. FifthLecture/01. Library/main.cpp
@@ -1,6 +1,8 @@
 #include "Author.h"
 #include "Book.h"
 
+#include <vector>
+
 int main() {
     Author a1("Harper", "Lee", "American");
     //a1.print();
@@ -8,11 +10,31 @@ int main() {
     Author a2("Stephen", "King", "American");
     //a2.print();
 
+    Author a3("Joanne", "Rowling", "British");
+    Author a4("Walter", "Isaacson", "American");
+    Author a5("Casey", "McQuiston", "American");
+
     Book b1("To Kill a Mockingbird", "", 1960, a1);
-    b1.print();
+    //b1.print();
 
     Book b2("Fairy Tale", "", 2022, a2);
-    b2.print();
+    //b2.print();
+
+    Book b3("Harry Potter and the Philosopher's Stone", "", 1997, a3);
+    Book b4("Steve Jobs", "", 2011, a4);
+    Book b5("Red, White & Royal Blue", "", 2019, a5);
+
+    std::vector<Book> libarary;
+    libarary.push_back(b1);
+    libarary.push_back(b2);
+    libarary.push_back(b3);
+    libarary.push_back(b4);
+    libarary.push_back(b5);
+
+    std::cout << "All available books inour library is: " << std::endl;
+    for (size_t i = 0; i < libarary.size(); ++i) {
+        libarary[i].print();
+    }
 
     return 0;
 }
