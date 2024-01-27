4589281139aafd4587531c8306ebfe63b51a5502
ZahariBakov <zaharibakov@gmail.com>
2024-01-27 09:25:13 +0200

    Create folder "OperatorOverloading". Adding exercise "exerciseOne". Operator Overloading "+" and "-".

---
+++ DevStyleR/C++ Programing Part 2/OperatorOverloading/exerciseOne/Box.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+
+class Box {
+    friend Box operator+(Box, Box);
+    friend Box operator-(Box, Box);
+public:
+    Box(int, int, int);
+
+    void setWidth(int);
+    void setHeigth(int);
+    void setLength(int);
+
+    int getWidth() const;
+    int getHeight() const;
+    int getLength() const;
+
+    void printBox();
+
+private:
+    int _width;
+    int _height;
+    int _length;
+};
